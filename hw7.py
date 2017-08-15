87d763e11390d3bb24125b09f5edd9ea8787a3e0
Melissa Chambers <phoneymc@gmail.com>
2017-08-15 15:56:54 -0700

    adding homework 7 to git

--- hw7.py
+++ hw7.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 '''
+working with sorted dictionary
+
 requires: python3
-add more detail to the docstring
+
 
 '''
 
 from sortedcontainers import SortedDict
+
 
 def print_menu():
     print('1. Print Users')
@@ -15,6 +18,7 @@ def print_menu():
     print('5. Quit')
     print()
 
+
 # Create dictionary with key = Names, value = user_name
 usernames = SortedDict()
 usernames['Summer'] = 'summerela'
@@ -23,24 +27,32 @@ usernames['Steven'] = 'LoLCat'
 usernames['Zara'] = 'zanyZara'
 usernames['Renato'] = 'songDude'
 
+
 # setup counter to store menu choice
 menu_choice = 0
 
-# display your menu
-##print_menu()
 
-## add error handler in case input is not a number
+# display your menu
+print_menu()
+
+
 # as long as the menu choice isn't "quit" get user options
 while menu_choice != 5:
     # get menu choice from user
-    print_menu()
-    menu_choice = int(input("Type in a number (1-5): "))
-    # add error handling if input is not number
+    # add error handler in case input is not a number
+    try:
+        menu_choice = int(input("Type in a number (1-5): "))
+    except ValueError:
+        print("\nYou must enter a number 1-5. Try again.\n")
+        print_menu()
+        continue
+
     # view current entries
     if menu_choice == 1:
-        print("Current Users:")
+        print("Current Users:\n")
         for x, y in usernames.items():
             print("Name: {} \tUser Name: {} \n".format(x, y))
+        print_menu()
 
     # add an entry
     elif menu_choice == 2:
@@ -48,48 +60,48 @@ while menu_choice != 5:
         name = input("Name: ")
         username = input("User Name: ")
         usernames[name] = username
-        ##ask teacher: how is the above working to append to the dict? - it's just appending without "append"
-        # print(key, value)
-        print(name, username, "has been added.\n")
+        print("\nUser added.\n")
+        print_menu()
 
     # remove an entry
     elif menu_choice == 3:
         print("Remove User")
         name = input("Name: ")
-        # if name not found, allow 3 more attempts to enter a valid name
+        # add while loop to give 3 attempts to enter valid user
         count = 0
         while name not in usernames and count < 3:
-            print("Invalid name. Try again.\n")
-            #print("Enter name.")
+            print("Invalid entry. Try again.\n")
             name = input("Name: ")
             count += 1
             continue
         if name in usernames:
             del usernames[name]
-            print("{} has been removed.\n".format(name))
+            print("\nUser removed.\n")
+            print_menu()
+            ### pass  # delete that entry
         else:
-            print("Invalid name. Unable to complete the request.\n")
+            print("Invalid entry. Unable to complete the request.\n")
+            print_menu()
 
     # view user name
     elif menu_choice == 4:
-        print("Lookup Username")
+        print("Lookup User")
         name = input("Name: ")
-        # same error handling as above
+        # add while loop to give 3 attempts to enter valid user
         count = 0
         while name not in usernames and count < 3:
-            print("Invalid name. Try again.\n")
-            #print("Enter a valid name.")
+            print("Invalid entry. Try again.\n")
             name = input("Name: ")
             count += 1
             continue
         if name in usernames:
-            print("Username is {}.\n".format(usernames[name]))
+            print("Username: ", usernames[name], "\n")
+            print_menu()
         else:
-            print("Invalid name. Unable to complete the request.\n")
+            print("Invalid entry. Unable to complete the request.\n")
+            print_menu()
 
-    # if user enters something strange, show them the menu
     # if user enters a number other than 1-5
     elif menu_choice != 5:
-        print("Invalid entry.\n")
-        continue
-        #print_menu()
+        print("Not a valid menu choice. Try again.\n")
+        print_menu()
